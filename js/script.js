ce16bf253ff05f5a5e7ddadbc715d8883d70332d
oninotna <antoninomusarratubbi@gmail.com>
2025-04-10 18:03:03 +0200

    finished inserting output variables

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 // * VARIABILI DI INPUT
-const userDate = document.getElementById("user-date");
+const userInfo = document.getElementById("user-info");
 const tripDistance = document.getElementById("trip-distance");
 const ageRange = document.getElementById("age-range");
 const formInput = document.getElementById("form-input");
 const generateButton = document.getElementById("generate-btn");
+const deleteButton = document.getElementById("delete-btn");
 const priceForKm = 0.21;
 
 
 // * VARIABILI DI OUTPUT
 let finalPrice = 0;
+const ticketUserInfo = document.getElementById("ticket-user-info");
+const ticketPlane = document.getElementById("ticket-plane");
+const ticketDiscount = document.getElementById("ticket-discount");
+const ticketTripDistance = document.getElementById("ticket-trip-distance");
+const ticketFinalPrice = document.getElementById("ticket-final-price");
+
 
 formInput.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -27,9 +34,48 @@ formInput.addEventListener("submit", (event) => {
         finalPrice = totalPrice;
     }
 
-    console.log(userDate.value);
+    ticketUserInfo.append(userInfo.value);
+    ticketPlane.append(ageRange.value);
+    ticketTripDistance.append(tripDistance.value);
+    ticketFinalPrice.append(`${finalPrice.toFixed(2)}€`);
+
+    console.log(userInfo.value);
     console.log(tripDistance.value);
     console.log(ageRange.value);
     console.log(totalPrice.toFixed(2)); 
     console.log(finalPrice.toFixed(2));   
 });
+
+deleteButton.addEventListener("click", () => {
+
+}); 
+
+
+
+
+
+// document.getElementById('ticket-discount').innerHTML = `
+//         <div>
+//         </div>
+//     `;
+
+
+//     const container = document.createElement('div')
+//     container.classList.add('container', 'pippo');
+
+//     const row = document.createElement('div');
+//     row.classList.add('row');
+
+//     for (let i = 0; i < 5; i++) {
+//         const col = document.createElement('div');
+//         col.classList.add('col');
+//         row.appendChild(col);
+//     }
+
+//     container.appendChild(row);
+//     ticketUserInfo.append(container);
+// function myCreateElement(tagName, classList) {
+//     const el = document.createElement(tagName);
+//     // ...
+//     return el;
+// }
